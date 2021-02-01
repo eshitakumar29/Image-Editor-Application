07ce6ccfc0c3354431507de7d35b5a0d1be4cb8b
eshitakumar29 <77023942+eshitakumar29@users.noreply.github.com>
2021-02-01 10:45:31 -0500

    Update a6image.py

--- a6image.py
+++ a6image.py
@@ -1,12 +1,5 @@
 """
 The main class for our imager application.
-
-This modules contains a single class.  Instances of this class support an image that can
-be modified.  This is the main class needed to display images in the viewer.
-
-Based on an original file by Dexter Kozen (dck10) and Walker White (wmw2)
-
-Author: Eshita Kumar (ek536) Sarika Kanna (sk2446)
 Date:   November 20, 2019
 """
 
@@ -29,7 +22,6 @@ def _is_pixel(item):
     return True
 
 
-# TASK 0: IMPLEMENT THIS HELPER
 def _is_pixel_list(data):
     """
     Returns True if data is a pixel list, False otherwise.
@@ -38,7 +30,7 @@ def _is_pixel_list(data):
     of 3 ints in the range 0..255
 
     Parameter data: The data to check
-    Precondition: NONE (data can be anything)(
+    Precondition: NONE (data can be anything)
     """
     if type(data) is list:
         for x in range(len(data)):
@@ -48,49 +40,11 @@ def _is_pixel_list(data):
     return False
 
 
-# TASK 1: IMPLEMENT THIS CLASS
 class Image(object):
     """
     A class that allows flexible access to an image pixel list
-
-    One of the things that we will see in this assignment is that sometimes
-    you want to treat an image as a flat 1D list and other times you want to
-    treat it as a 2D list. This class has methods that allow you to go back
-    and forth between the two.
-
-    If you want to treat the image like a 2D list, you use the methods
-    `getPixel` and `setPixel`. As with the Pixels class, pxels are represented
-    as 3-element tuples, with each element in the range 0..255.  For example,
-    red is (255,0,0). These methods are used by many of the Instagram-style
-    filter functions.
-
-    If you want to treat the image like a 1D list you just use list brackets
-    like it was a normal list:
-
-        image[pos] = (255,0,0)
-
-    The methods `__getitem__` and `__getitem__` provide operator overloading for [].
-    So the call above is the same as the image call
-
-        image.__setitem__(pos, (255,0,0))
-
-     These operations are used by the greyscale filters and the stenography methods.
     """
-    # IMMUTABLE ATTRIBUTES (Fixed after initialization)
-    # Attribute _data: The underlying list of pixels
-    # Invariant: _data is a pixel list (see _is_pixel_list)
-    #
-    # MUTABLE ATTRIBUTES (Can be changed at any time, via the setters)
-    # Attribute _width:  The image width, which is the number of columns
-    # Invariant: _width is an int > 0, _width*_height = len(_data)
-    # width = 0 only if len(_data) = 0
-    #
-    # Attribute _height:  The image height, which is the number of rows
-    # Invariant: _height is an int > 0, _width*_height = len(_data)
-    # height = 0 only if len(_data) = 0
-    # Note that if you change width, you must change height (to satisfy the invariant)
 
-    # PART A
     # GETTERS AND SETTERS
     def getData(self):
         """
@@ -105,21 +59,12 @@ class Image(object):
         """
         Returns the image width
 
-        A value width is an int evenly dividing the number of pixels in the
-        image. Width can only be 0 if the image is empty.
         """
         return self._width
 
     def setWidth(self,value):
         """
         Sets the image width to value, assuming it is valid.
-
-        If the width changes, then height must change to so that we preserve
-        width*height == # of pixels. This can only happen if the value is valid.
-
-        The value is valid if it is an int and it evenly divides the number of
-        pixels in the image. If the pixel list has 10 pixels, a valid width is
-        1, 2, 5, or 10. Width can only be 0 if the image is empty.
 
         Parameter value: the new width value
         Precondition: value is a valid width >= 0
@@ -139,22 +84,12 @@ class Image(object):
     def getHeight(self):
         """
         Returns the image height
-
-        A value height is an int evenly dividing the number of pixels in the
-        image. Height can only be 0 if the image is empty.
         """
         return self._height
 
     def setHeight(self,value):
         """
         Sets the image height to value, assuming it is valid.
-
-        If the height changes, then width must change to so that we preserve
-        width*height == # of pixels. This can only happen if the value is valid.
-
-        The value is valid if it is an int and it evenly divides the number of
-        pixels in the image. If the pixel list has 10 pixels, a valid height is
-        1, 2, 5, or 10. Height can only be 0 if the image is empty.
 
         Parameter value: the new height value
         Precondition: value is a valid height >= 0
@@ -175,23 +110,6 @@ class Image(object):
         """
         Initializes an Image from the given pixel list.
 
-        A pixel list is a 1-dimensional list of pixels where a pixel is a
-        tuple of 3 ints in the range 0..255. The pixel list contains the
-        image data. You do not need to worry about loading an image file.
-        That happens elsewhere in the application (in code that you did not
-        write).
-
-        However, in order to be valid, the width  must evenly divide the
-        number of pixels in the image. So if the pixel list has 10 pixels, a
-        valid width is 1, 2, 5, or 10.
-
-        The height is not given explicitly, but you must compute it from the
-        width and pixel list length.
-
-        This initializer stores a reference to the original image data; it
-        does not copy it. So changes to the image will change the data
-        parameter as well.
-
         Parameter data: The image data as a pixel list
         Precondition: data is a pixel list
 
@@ -203,7 +121,6 @@ class Image(object):
         self.setWidth(width)
         self.setHeight(len(self.getData())//width)
 
-    # PART B
     # OPERATOR OVERLOADING
     def __len__(self):
         """
@@ -216,13 +133,6 @@ class Image(object):
     def __getitem__(self, pos):
         """
         Returns the pixel at the given position.
-
-        This special method supports the [] operator for accessing pixels.
-        It is better than direct access because it enforces its precondition.
-
-        This method is used when you want to treat an image as a flat,
-        one-dimensional list rather than a 2-dimensional image. It is useful
-        for the steganography part of the assignment.
 
         The value returned is a 3-element tuple (r,g,b).
 
@@ -238,9 +148,6 @@ class Image(object):
         """
         Sets the pixel at the given position to the given value.
 
-        This special method supports the [] operator for accessing pixels.
-        It is better than direct access because it enforces its precondition.
-
         Parameter pos: The position in the pixel list
         Precondition: pos is an int and a valid position >= 0 in the pixel list.
 
@@ -254,14 +161,10 @@ class Image(object):
         list = self._data
         list[pos] = pixel
 
-    # PART C
     # TWO-DIMENSIONAL ACCESS METHODS
     def getPixel(self, row, col):
         """
         Returns a copy of the pixel value at (row, col)
-
-        Remember that this way of accessing a pixel is essentially (y,x) since
-        height is the number of rows and width is the number of columns.
 
         The value returned is a 3-element tuple (r,g,b).
 
@@ -280,9 +183,6 @@ class Image(object):
         """
         Sets the pixel value at (row, col) to (a copy of) pixel
 
-        Remember that this way of setting a pixel is essentially (y,x) since
-        height is the number of rows and width is the number of columns.
-
         Parameter row: The pixel row
         Precondition: row is an int >= 0 and < height
 
@@ -296,13 +196,10 @@ class Image(object):
         assert (type(col) is int) and (col >= 0) and (col < self.getWidth())
         assert _is_pixel(pixel)
 
-
         list = self._data
         copy_pixel = pixel
         list[row*self.getWidth() + col%self.getWidth()] = copy_pixel
 
-
-    # PART D
     def __str__(self):
         """
         Returns: The string representation of this image.
@@ -312,24 +209,6 @@ class Image(object):
 
             [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 0, 0), (128, 0, 0), (0, 128, 0)]
 
-        If the width (which is the number of columns) is two, the string
-        should be
-
-            '[[(255, 0, 0), (0, 255, 0)],\n[(0, 0, 255), (0, 0, 0)],\n[(128, 0, 0), (0, 128, 0)]]
-
-        Note the newlines (\n) after each row. That is because when you print this
-        string, it will look like this:
-
-            [[(255, 0, 0), (0, 255, 0)],
-            [(0, 0, 255), (0, 0, 0)],
-            [(128, 0, 0), (0, 128, 0)]]
-
-        This is useful for debugging, since it allows us to see each row of the
-        image on its own line.
-
-        There should be spaces after the commas but no where else. Tuples
-        (the individual pixels) handle this  part for you automatically, but you
-        need to handle the commas between pixels and the newlines between rows.
         """
         string_rep = '['
         a = 0
@@ -368,8 +247,6 @@ class Image(object):
         Parameter col2: The pixel column to swap to
         Precondition: col2 is an int >= 0 and < width
         """
-        # NOTE: DO NOT enforce any preconditions here.
-        # They should be enforced already in getPixel and setPixel.
         temp = self.getPixel(row1,col1)
         self.setPixel(row1,col1,self.getPixel(row2,col2))
         self.setPixel(row2,col2,temp)
@@ -377,12 +254,8 @@ class Image(object):
     def copy(self):
         """
         Returns a copy of this image object.
-
-        The underlying pixel data must be copied (e.g. the copy cannot refer
-        to the same list of pixels that this object does).
         """
         copy_obj = Image(self.getData(),self.getWidth())
         copy_obj._data = self.getData()[:]
         copy_obj._width = self.getWidth()
-        # copy_obj._height = self.getHeight()
         return copy_obj
